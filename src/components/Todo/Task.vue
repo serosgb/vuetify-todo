9d9075a5accfea25803f192795e1a960e1828339
guities <sergio.guitierrezbonilla@vw.com>
2021-03-02 15:22:04 -0500

    Adding task menu and wiring delete option

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -1,62 +1,48 @@
 <template>
-        <div >
-            <v-list-item
-                @click="doneTask(task.id)"
-                :class="{ 'blue lighten-5': task.done }"
-            >
-                <template v-slot:default>
-                    <v-list-item-action>
-                        <v-checkbox
-                            :input-value="task.done"
-                            color="primary"
-                        ></v-checkbox>
-                    </v-list-item-action>
+    <div>
+        <v-list-item
+            @click="doneTask(task.id)"
+            :class="{ 'blue lighten-5': task.done }"
+        >
+            <template v-slot:default>
+                <v-list-item-action>
+                    <v-checkbox
+                        :input-value="task.done"
+                        color="primary"
+                    ></v-checkbox>
+                </v-list-item-action>
 
-                    <v-list-item-content>
-                        <v-list-item-title
-                            :class="{
-                                'text-decoration-line-through': task.done,
-                            }"
-                            >{{ task.title }}</v-list-item-title
-                        >
-                    </v-list-item-content>
+                <v-list-item-content>
+                    <v-list-item-title
+                        :class="{
+                            'text-decoration-line-through': task.done,
+                        }"
+                        >{{ task.title }}</v-list-item-title
+                    >
+                </v-list-item-content>
 
-                    <v-list-item-action>
-                        <v-btn icon @click.stop="dialogs.delete = true">
-                            <v-icon color="primary lighten-1"
-                                >mdi-delete</v-icon
-                            >
-                        </v-btn>
-                    </v-list-item-action>
-                </template>
-            </v-list-item>
-            
-            <v-divider></v-divider>
+                <v-list-item-action>
+                    <task-menu :task="task" />
+                </v-list-item-action>
+            </template>
+        </v-list-item>
 
-            <dialog-delete v-if="dialogs.delete" @close="dialogs.delete = false" :task="task"/>
-        </div>
+        <v-divider></v-divider>
+    </div>
 </template>
 
 <script>
 import { mapMutations } from 'vuex';
-import DialogDelete from '@/components/Todo/Dialogs/DialogDelete';
+import TaskMenu from '@/components/Todo/TaskMenu';
 
 export default {
     name: 'Task',
     components: {
-        DialogDelete
+        TaskMenu,
     },
     props: ['task'],
-    data() {
-        return{
-            dialogs: {
-                delete: false
-            }
-        }
-    },
     methods: {
-        ...mapMutations(['doneTask'])
-    }    
-}
+        ...mapMutations(['doneTask']),
+    },
+};
 </script>
-
---
+++ src/components/Todo/TaskMenu.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <v-menu bottom left>
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn color="primary" icon v-bind="attrs" v-on="on">
+                    <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item
+                  v-for="(item, index) in items"
+                  :key="index"
+                  @click="handleClick(index)">
+                    <v-list-item-icon>
+                        <v-icon v-text="item.icon"></v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        <dialog-delete v-if="dialogs.delete" @close="dialogs.delete = false" :task="task"/>
+    </div>
+</template>
+
+<script>
+import DialogDelete from '@/components/Todo/Dialogs/DialogDelete';
+
+export default {
+    name: 'TaskMenu',
+    components: {
+        DialogDelete,
+    },
+    props: ['task'],
+    data() {
+        return {
+            dialogs: {
+                delete: false
+            },            
+            items: [
+                {
+                    title: 'Edit',
+                    icon: 'mdi-pencil',
+                    click(){
+                        console.log('Click on edit')
+                    }
+                },
+                { 
+                    title: 'Due date',
+                    icon: 'mdi-calendar',
+                    click(){
+                        console.log('Click on due date')
+                    }
+                },
+                { 
+                    title: 'Delete',
+                    icon: 'mdi-delete',
+                    click() {
+                        this.dialogs.delete = true;
+                    }
+                }
+            ]            
+        }
+    },
+    methods: {
+        handleClick(index) {
+            this.items[index].click.call(this);
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
